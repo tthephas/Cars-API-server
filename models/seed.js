fadc3f9ba29eaa25f64aa952757361836bd7f7df
thepjos <josh.thephasdin@gmail.com>
2023-01-20 08:50:38 -0600

    trying to fix seed data

--- models/seed.js
+++ models/seed.js
@@ -10,7 +10,7 @@ const Cars = require('./cars')
 const db = mongoose.connection
 
 // we're going to build a seed route
-db.on('open', (req, res) => {
+db.on('open', () => {
     // array of starter resources- cars 
     const startCars = [
         {make: 'Tesla', model: 'Model 3', color: 'Grey', forSale: false},
@@ -22,17 +22,20 @@ db.on('open', (req, res) => {
     // then we delete every fruit in the database(all instances of this resource)
     Cars.deleteMany({ owner: null})
         .then(() => {
-    Cars.create(startCars)
-        .then(data => {
-            console.log('Here are the created cars:', data)
-            db.close()
+            Cars.create(startCars)
+                .then(data => {
+                    console.log('Here are the created cars:', data)
+                    db.close()
+                })
+                    .catch(err => {console.log('The following error occured: \n', err)
+                    db.close()
+                })
+        .catch(err => {
+                console.log(err)
+                // always make sure to close the connection
+                db.close()
         })
-        .catch(err => console.log('The following error occured: \n', err))
-        db.close()
     })
 })
-.catch(err => {
-    console.log(err)
-    // always make sure to close the connection
-    db.close()
-})
+
+
